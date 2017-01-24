d9b5a76e9bcafccf911c09e12a791f7cd07fabb8
Emil <Emil@ABSYSCO-EMIL>
2017-01-24 11:18:32 +0100

    Reduced Code and Improved Maintainability with Data Driven Tests

---
+++ xUnit/DemoCode.Tests/CsvTestDataAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace DemoCode.Tests
+{
+    public class CsvTestDataAttribute : DataAttribute
+    {
+        private readonly string _csvFileName;
+
+        public CsvTestDataAttribute(string csvFileName)
+        {
+            _csvFileName = csvFileName;
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            var csvLinses = File.ReadAllLines(_csvFileName);
+
+            var testCases = new List<object[]>();
+
+            foreach (var csvLinse in csvLinses)
+            {
+                var values = csvLinse.Split(',').Select(int.Parse);
+
+                var testCase = values.Cast<object>().ToArray();
+
+                testCases.Add(testCase);
+            }
+
+            return testCases;
+        }
+    }
+}
--- xUnit/DemoCode.Tests/MemoryCalculatorTests.cs
+++ xUnit/DemoCode.Tests/MemoryCalculatorTests.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using Ploeh.AutoFixture.Xunit2;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -16,6 +22,33 @@ namespace DemoCode.Tests
         public void Dispose()
         {
             Sut.Dispose();
+        }
+    }
+
+    public class MemoryCalculatorTestData
+    {
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+//                yield return new object[] { 5, 10, -15 };
+//                yield return new object[] { -2, -1, 3 };
+
+                var csvLinses = File.ReadAllLines("TestData.csv");
+
+                var testCases = new List<object[]>();
+
+                foreach (var csvLinse in csvLinses)
+                {
+                    var values = csvLinse.Split(',').Select(int.Parse);
+
+                    var testCase = values.Cast<object>().ToArray();
+
+                    testCases.Add(testCase);
+                }
+
+                return testCases;
+            }
         }
     }
 
@@ -58,10 +91,41 @@ namespace DemoCode.Tests
             Assert.Equal(-5, _fixture.Sut.CurrentValue);
         }
 
-//        public void Dispose()
-//        {
-//            _testOutput.WriteLine("Disposing sut");
-//            _sut.Dispose();
-//        }
+        [Theory]
+//        [InlineData(5, 10, -15)]
+//        [InlineData(-2, -1, 3)]
+//        [MemberData(nameof(MemoryCalculatorTestData.TestData), MemberType=typeof(MemoryCalculatorTestData))]
+        [CsvTestData("TestData.csv")]
+        public void ShouldSubtractTwoNumbers(int firstNumber, int secondNumber, int expectedResult)
+        {
+            _testOutput.WriteLine("Executing ShouldSubtractTwoNumbers");
+
+            _fixture.Sut.Subtract(firstNumber);
+            _fixture.Sut.Subtract(secondNumber);
+
+            Assert.Equal(expectedResult, _fixture.Sut.CurrentValue);
+        }
+
+        [Theory]
+        //[AutoData]
+        [InlineAutoData()]
+        [InlineAutoData(-5)]
+        public void ShouldAddTwoNumbersAuto(int firstNumber, int secondNumber, MemoryCalculator sut)
+        {
+            _testOutput.WriteLine("Executing ShouldAddTwoNumbersAuto");
+
+            sut.Add(firstNumber);
+            _testOutput.WriteLine("firstNumber: " + firstNumber);
+            sut.Add(secondNumber);
+            _testOutput.WriteLine("secondNumber: " + secondNumber);
+
+            Assert.Equal(firstNumber + secondNumber, sut.CurrentValue);
+        }
+
+        //        public void Dispose()
+        //        {
+        //            _testOutput.WriteLine("Disposing sut");
+        //            _sut.Dispose();
+        //        }
     }
 }
